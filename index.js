2e718151aa83480d6ffadd491e3d35e4bc979d3e
jaredhensley <jhensley@examsoft.com>
2020-12-09 15:54:01 -0600

    feat(audible-alert): added an audible voice alert to notify users when the browser opens

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-const open = require('open');
-const promptly = require('promptly');
+const open = require("open");
+const promptly = require("promptly");
+const { exec } = require("child_process");
 
-const {checkForPlaystationDirectRedirect} = require("./utils");
+const { checkForPlaystationDirectRedirect } = require("./utils");
 
 /** Constants */
 let numTries = 1;
 const playstationType = {
-    "disc": {
-        "id": 3005816,
-        "url": "https://direct.playstation.com/en-us/consoles/console/playstation5-console.3005816",
-    },
-    "digital": {
-        "id": 3005817,
-        "url": "https://direct.playstation.com/en-us/consoles/console/playstation5-digital-edition-console.3005817",
-    }
+  disc: {
+    id: 3005816,
+    url:
+      "https://direct.playstation.com/en-us/consoles/console/playstation5-console.3005816",
+  },
+  digital: {
+    id: 3005817,
+    url:
+      "https://direct.playstation.com/en-us/consoles/console/playstation5-digital-edition-console.3005817",
+  },
 };
 
 /** Let's do this */
-(async function() {
-    const choice = await promptly.choose("Which version would you like? (disc or digital)", ["disc", "digital"]);
-    console.log(`Searching for PlayStation 5 ${choice} edition...`);
-    
-    const onSuccess = () => {
-        console.log("Found it! Opening queue now...");
-        open(playstationType[choice].url);
-    };
+(async function () {
+  const choice = await promptly.choose(
+    "Which version would you like? (disc or digital)",
+    ["disc", "digital"]
+  );
+  console.log(`Searching for PlayStation 5 ${choice} edition...`);
 
-    checkForPlaystationDirectRedirect(5000, onSuccess);
+  const onSuccess = () => {
+    console.log("Found it! Opening queue now...");
+    exec("say 'queue is up. solve those captchas! run!'");
+    open(playstationType[choice].url);
+  };
+
+  checkForPlaystationDirectRedirect(5000, onSuccess);
 })();
