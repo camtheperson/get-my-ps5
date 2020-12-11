5969dd96b447599e6ecd2a032079595d073c5aa3
Alex Sheehan <alex.sheehan@rallyhealth.com>
2020-12-11 15:06:16 -0800

    Fire if cookie 'Queue-it' is found

--- src/utils.js
+++ src/utils.js
@@ -38,9 +38,7 @@ function addToCartLoop(id, guid, numTries, checkInterval = 10000) {
  * @param onSuccess - Callback function for successful redirect
  */
 async function checkForPlaystationDirectRedirect(checkInterval, onSuccess, version, browser, numTries = 1) {
-    let response;
-    let responseBody;
-    let responseStatus;
+    let cookies, response, responseBody, responseStatus;
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage();
     const url = `https://direct.playstation.com/en-us/consoles/console/playstation5-console.${version}`;
@@ -49,6 +47,7 @@ async function checkForPlaystationDirectRedirect(checkInterval, onSuccess, versi
         response = await page.goto(url);
         responseBody = await response.text();
         responseStatus = await response.status();
+        cookies = await page.cookies();
     } catch(err) {
         console.log("Error connecting to PlayStation Direct store.");
     }
@@ -58,8 +57,8 @@ async function checkForPlaystationDirectRedirect(checkInterval, onSuccess, versi
     // console.log(`Response body: ${responseBody}`);
     // console.log(`Response status: ${responseStatus}`);
 
-    if (responseBody && responseBody.indexOf("queue-it_log") > 0 && 
-        responseBody.indexOf("softblock") === -1) {
+    if ((responseBody && responseBody.indexOf("queue-it_log") > 0 && 
+        responseBody.indexOf("softblock") === -1) || (cookies && cookies.length && cookies.some(cookie => cookie.name === 'Queue-it'))) {
         onSuccess();
     } else {
         setTimeout(() => {
