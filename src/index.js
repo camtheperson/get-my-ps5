d335508f20a3fe6638e5e5248e482231123d24b3
outphase84 <outphase84@gmail.com>
2020-12-13 15:18:14 -0500

    walmart watcher tested looks good -- still beta though

--- src/index.js
+++ src/index.js
@@ -2,24 +2,29 @@ const open = require("open");
 const promptly = require("promptly");
 const puppeteer = require("puppeteer");
 
-const {checkForPlaystationDirectRedirect, playAlarm} = require("./utils");
+const {watchWalmart, checkForPlaystationDirectRedirect, playAlarm} = require("./utils");
 
 /** Constants */
 const playstationType = {
     disc: {
         id: 3005816,
         url: "https://direct-queue.playstation.com/?c=sonyied&e=psdirectprodku1&t=https%3A%2F%2Fdirect.playstation.com%2Fen-us%2Fconsoles%2Fconsole%2Fplaystation5-console.3005816&cv=1089561812&cid=en-US",
+        upc: "711719541028",
+        walmartUrl: "https://affil.walmart.com/cart/buynow?items=363472942"
     },
     digital: {
         id: 3005817,
         url: "https://direct-queue.playstation.com/?c=sonyied&e=psdirectprodku1&t=https%3A%2F%2Fdirect.playstation.com%2Fen-us%2Fconsoles%2Fconsole%2Fplaystation5-digital-edition-console.3005817&cid=en-US",
+        upc: "711719541035",
+        walmartUrl: "https://affil.walmart.com/cart/buynow?items=493824815"
     }
 };
 
 /** Let's do this */
 (async function() {
     const choice = await promptly.choose("Which version would you like? (disc or digital)", ["disc", "digital"]);
-    const alarm = await promptly.choose("Would you like to hear a loud, annoying alarm when we find your PS5? (Y or N)", ["Y", "N"])
+    const alarm = await promptly.choose("Would you like to hear a loud, annoying alarm when we find your PS5? (Y or N)", ["Y", "N", "y", "n"]);
+    const storeId = await promptly.prompt("Enter your local Walmart store number.  If you're not sure, just leave this blank.", {default: "1"});
     console.log(`Searching for PlayStation 5 ${choice} edition...`);
     
     const onSuccess = () => {
@@ -29,7 +34,17 @@ const playstationType = {
             playAlarm();
         }
     };
+
+    const onWalmartSuccess = () => {
+        console.log("Walmart in stock!  Opening add to cart link now...");
+        open(playstationType[choice].walmartUrl);
+        if (alarm.toUpperCase === "Y") {
+            playAlarm();
+        }
+    }
+
     checkForPlaystationDirectRedirect(5000, onSuccess, playstationType[choice].id, await puppeteer.launch());
+    watchWalmart(5000, onWalmartSuccess, playstationType[choice].upc, storeId);
 })();
 
 // Gracefully exit
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
 const axios = require("axios").default;
 const cmd = require("node-cmd");
+const superagent = require("superagent");
+
 
 /** addToCartLoop 
  * Recursively tries to add a product to the cart
@@ -63,15 +65,79 @@ async function checkForPlaystationDirectRedirect(checkInterval, onSuccess, versi
         onSuccess();
     } else {
         setTimeout(() => {
-            console.log("No stock detected yet. Scanning again...");
-            console.log("Number of tries", numTries);
-            console.log("");
+            console.log("PS Direct: No queue detected yet. Scanning again...");
+            console.log(`Number of tries: ${numTries}\n`);
             numTries++;
 
             checkForPlaystationDirectRedirect(checkInterval, onSuccess, version, browser, numTries);
         }, checkInterval);
     }
 }
+
+async function watchWalmart(checkInterval, onWalmartSuccess, version, storeId, numWalmartTries = 1) {
+    let walmartStockStatus = await checkWalmartStock(version, storeId);
+    let walmartRetry;
+
+    switch(walmartStockStatus) {
+        case "walmartConnectionError":
+            console.log("Walmart Connection Error");
+            console.log(`Number of tries: ${numWalmartTries}\n`);
+            walmartRetry = true;
+            break;
+        case "walmartOnlineFound":
+            console.log("Found Walmart Online Inventory!");
+            onWalmartSuccess();
+            break;
+        case "walmartInStoreFound":
+            console.log("Found Walmart Store Pickup Inventory!");
+            onWalmartSuccess();
+            break;
+        case "noPage":
+            console.log("Product page not up yet.");
+            console.log(`Walmart Number of tries: ${numWalmartTries}`);
+            walmartRetry = true;
+            break;
+        default:
+            console.log("Walmart: No inventory detected yet.  Scanning again...");
+            console.log(`Walmart Number of tries: ${numWalmartTries}\n`);
+            walmartRetry = true;
+            break;
+    }
+    if (walmartRetry == true) {
+        numWalmartTries++;
+        setTimeout(() => {
+            watchWalmart(checkInterval, onWalmartSuccess, version, storeId, numWalmartTries);
+        }, checkInterval);
+    }
+}
+
+function checkWalmartStock(version, storeId) {
+    return new Promise(async (resolve, reject) => {
+        let testUrl = `https://putsreq.com/dp9X62F4g5Fj37MJ6aX0?version=${version}?storeId=${storeId}`;
+        let prodUrl = `https://search.mobile.walmart.com/v1/products-by-code/UPC/${version}?storeId=${storeId}`;
+        superagent.get(prodUrl)
+            .end((err, res) => {
+                if (err) {
+                    if (res.status == 444) {
+                        return resolve("noPage");
+                    } else {
+                    return resolve("walmartConnectionError");
+                    }
+            }
+            superagent.parse['application/json']
+            if (res.body.data.inStore && res.body.data.inStore.inventory.available == true) {
+                return resolve("walmartInStoreFound");
+            } else if (res.body.data.online.inventory && res.body.data.online.inventory.available == true) {
+                return resolve("walmartOnlineFound");
+            } else {
+                return resolve("noInventory");
+            }
+        })            
+            
+    })
+
+};
+
 
 function playAlarm() {
     const os = process.platform;
@@ -100,4 +166,6 @@ module.exports = {
     checkForPlaystationDirectRedirect,
     getGuid,
     playAlarm,
+    checkWalmartStock,
+    watchWalmart
 }
